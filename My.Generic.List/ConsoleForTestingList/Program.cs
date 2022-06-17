7a72bc02301b42406ce9d4c29438c5265edb499a
elda-gomes <elda.dimm@gmail.com>
2022-06-17 09:00:09 -0300

    Impementing my own generic list just for fun

---
+++ My.Generic.List/ConsoleForTestingList/Program.cs
@@ -0,0 +1,45 @@
+﻿using My.Generic.List;
+using System;
+
+namespace ConsoleForTestingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing my generic list \\o/");
+            Console.WriteLine();
+
+            var listOfNumbers = new List<int>();
+
+            Console.WriteLine("Adding a number...");
+            listOfNumbers.Add(0);
+            ShowListItems(listOfNumbers);
+
+            Console.WriteLine("Adding a range of numbers...");
+            listOfNumbers.AddRange(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            ShowListItems(listOfNumbers);
+
+            Console.WriteLine("Removing number 5...");
+            listOfNumbers.Remove(5);
+            ShowListItems(listOfNumbers);
+
+            Console.WriteLine("Adding number 5...");
+            listOfNumbers.Add(5);
+            ShowListItems(listOfNumbers);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowListItems(List<int> items)
+        {
+            Console.WriteLine();
+            Console.WriteLine("List items:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"- {i}: {items[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ My.Generic.List/My.Generic.List/List.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace My.Generic.List
+{
+    public class List<T>
+    {
+        private T[] _listItems;
+        private int _index;
+
+        public int Length => _index;
+
+        public List()
+        {
+            this._listItems = new T[5];
+            this._index = 0;
+        }
+
+        public void AddRange(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            CheckArraySize();
+
+            this._listItems[this._index] = item;
+            this._index++;
+        }
+
+        private void CheckArraySize()
+        {
+            if (this._index >= this._listItems.Length)
+            {
+                T[] transitoryArray = new T[this._index * 2];
+                Array.Copy(sourceArray: this._listItems, destinationArray: transitoryArray, length: this._index);
+                this._listItems = transitoryArray;
+            }
+        }
+
+        public void Remove(T item)
+        {
+            var removingIndex = IndexOf(item);
+
+            if (removingIndex == -1)
+            {
+                return;
+            }
+
+            for (int i = removingIndex; i < this._index-1; i++)
+            {
+                this._listItems[i] = this._listItems[i + 1];
+            }
+
+            this._index--;
+        }
+
+        public int IndexOf(T item)
+        {
+            var index = -1;
+
+            for (int i = 0; i < this._index; i++)
+            {
+                var listItem = this._listItems[i];
+                if (listItem.Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        public T this[int index] => GetItem(index);
+
+        private T GetItem(int index)
+        {
+            if (index < this._index)
+            {
+                return this._listItems[index];
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
